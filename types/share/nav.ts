6e6e49a496bdd1b2359518f990224a159ab23c49
touraye <touraye07@gmail.com>
2025-08-12 19:32:06 +0000

    add shared types

---
+++ types/share/nav.ts
@@ -0,0 +1,21 @@
+// src/lib/navData.ts
+
+export interface NavLink {
+	href: string
+	label: string
+}
+
+export interface SubMenuColumn {
+	links: NavLink[]
+}
+
+export interface SubMenu {
+	title: string
+	columns: SubMenuColumn[]
+}
+
+export interface NavigationItem {
+	href: string
+	label: string
+	submenu?: SubMenu
+}
