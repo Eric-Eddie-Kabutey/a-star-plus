25ee69ebabce800c64ad2ca874080c6156321127
touraye <touraye07@gmail.com>
2025-08-12 19:11:02 +0000

    add commercial site data

---
+++ lib/data/com/service.ts
@@ -0,0 +1,28 @@
+import { KeyReasonsData } from "@/types/com/service";
+
+export const commercialPestControlMattersData: KeyReasonsData = {
+  tagline: 'WHY IT MATTERS',
+  title: 'Why Commercial Pest Control Matters',
+  description: 'Our platform helps your business in managing expenses. These are some of the reasons why you should use our platform in managing business finances.',
+  reasons: [
+    {
+      icon: "/assets/com/icons/blue-bug.png",
+      title: 'Customer Retention Risk',
+      description: '77% of customers won’t return after seeing a pest in a business',
+    },
+    {
+      icon: "/assets/com/icons/blue-bug.png",
+      title: 'The Economic Impact of Pest Issues',
+      description: 'Businesses lose an estimated $13.2 billion annually due to pest-related issues',
+    },
+    {
+      icon: "/assets/com/icons/blue-bug.png",
+      title: 'The Legal Liability of Pest Neglect',
+      description: 'Local compliance violations can lead to fines and closures',
+    },
+  ],
+  imageUrl: '/assets/com/images/services.png',
+  imageAlt: 'Pest control professional in a hazmat suit treating an office space.',
+};
+
+// industry solution data
