e74757ef53c5db3c1c4d07e0c21353a7f5ca2289
touraye <touraye07@gmail.com>
2025-08-15 00:34:55 +0000

    add share data for faq and testimonial

---
+++ lib/data/shared/faq.ts
@@ -0,0 +1,21 @@
+import { FaqProps } from "@/types/share/faq";
+
+// --- Data for the FAQ items ---
+export const faqItems: FaqProps[] = [
+  {
+    question: "How much does your service cost?",
+    answer: "Our service costs vary depending on the type and extent of the infestation, as well as the size of your property. We offer a free, no-obligation inspection and quote to provide you with an accurate price.",
+  },
+  {
+    question: "How quickly do you respond?",
+    answer: "We pride ourselves on our fast response times. For urgent situations, we offer same-day service to address your pest problems as quickly as possible and restore your peace of mind.",
+  },
+  {
+    question: "Are your treatments safe?",
+    answer: "Yes, the safety of your family and pets is our top priority. We use eco-friendly and industry-approved treatments that are highly effective against pests while being safe for use in homes and businesses.",
+  },
+  {
+    question: "What Pests do you treat?",
+    answer: "We treat a wide variety of common pests, including termites, roaches, ants, spiders, rodents, bed bugs, mosquitoes, and more. If you have a pest problem, chances are we have a solution for it.",
+  },
+];
