3b6ecd6bc90bdd4d6463a5352824b8cb13039a5b
touraye <touraye07@gmail.com>
2025-08-18 18:13:37 +0000

    update image path

--- lib/data/residential/service.ts
+++ lib/data/residential/service.ts
@@ -7,19 +7,19 @@ export interface ServiceCardData {
 
 export const servicesData: ServiceCardData[] = [
   {
-    imageSrc: '/assets/images/service-business.png',
+    imageSrc: '/assets/residential/images/service-business.png',
     title: 'Extensive Pest Control For Businesses',
     description: 'Comprehensive pest control solutions to protect your business, ensuring healthy environment and maximum productivity.',
     learnMoreLink: '#',
   },
   {
-    imageSrc: '/assets/images/service-home.png',
+    imageSrc: '/assets/residential/images/service-home.png',
     title: 'Extensive Home Fumigation',
     description: 'Thorough and effective home fumigation to eliminate pests at all stages, ensuring a safe and pest-free living space.',
     learnMoreLink: '#',
   },
   {
-    imageSrc: '/assets/images/service-urgent.png',
+    imageSrc: '/assets/residential/images/service-urgent.png',
     title: 'Fast Response to Urgent Pest Situation',
     description: 'Quick and effective pest control when you need it most. A star Plus pest control responds fast to eliminate urgent infestations and restore your peace of mind.',
     learnMoreLink: '#',
