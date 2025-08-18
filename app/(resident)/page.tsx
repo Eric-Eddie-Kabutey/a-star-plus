cd0a8cf7fba5368d7a34860f2eb3fe4a8450e1a0
touraye <touraye07@gmail.com>
2025-08-18 15:30:16 +0000

    pass data to faq

--- app/(resident)/page.tsx
+++ app/(resident)/page.tsx
@@ -1,32 +1,39 @@
-import { AboutUs } from "@/components/residential/home/about";
-import { Faq } from "@/components/shared/faq";
-import { Hero } from "@/components/residential/home/hero";
-import { PestTypes } from "@/components/residential/home/pets-types";
-import { Services } from "@/components/residential/home/service";
-import { Testimonials } from "@/components/shared/testimonials";
-import { WhyChooseUs } from "@/components/residential/home/why-choose-us";
+import { AboutUs } from '@/components/residential/home/about'
+import { Faq } from '@/components/shared/faq'
+import { Hero } from '@/components/residential/home/hero'
+import { PestTypes } from '@/components/residential/home/pets-types'
+import { Services } from '@/components/residential/home/service'
+import { Testimonials } from '@/components/shared/testimonials'
+import { WhyChooseUs } from '@/components/residential/home/why-choose-us'
+import { faqItems } from '@/lib/data/shared/faq'
 
 export default function Home() {
-  return (<>
-    {/* hero section */}
-    <Hero />
+	return (
+		<>
+			{/* hero section */}
+			<Hero />
 
-    {/* our service section */}
-    <Services />
+			{/* our service section */}
+			<Services />
 
-    {/* pets list section */}
-    <PestTypes />
+			{/* pets list section */}
+			<PestTypes />
 
-    {/* about us section */}
-    <AboutUs />
+			{/* about us section */}
+			<AboutUs />
 
-    {/* client's testimonials */}
-    <Testimonials />
+			{/* client's testimonials */}
+			<Testimonials />
 
-    {/* why choose us - happy customers */}
-    <WhyChooseUs /> 
+			{/* why choose us - happy customers */}
+			<WhyChooseUs />
 
-    {/* FAQ */}
-    <Faq />
-    </>);
+			{/* FAQ */}
+			<Faq
+				title={<>Everything You Need <br /> to Know</>}
+				hint='All Questions'
+				faqItems={faqItems}
+			/>
+		</>
+	)
 }
