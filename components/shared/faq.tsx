7f2f3a48b5633acd2dba74ec710d543d999eda41
touraye <touraye07@gmail.com>
2025-08-18 15:28:40 +0000

    make faq reusable and fix spacing

--- components/shared/faq.tsx
+++ components/shared/faq.tsx
@@ -9,7 +9,14 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { faqItems } from '@/lib/data/shared/faq';
+import { FaqProps } from '@/types/share/faq';
+import React from 'react';
+
+interface FAQProps {
+  title: React.ReactNode;
+  hint?: string;
+  faqItems: FaqProps[];
+}
 
 
 const leftColumnVariants: Variants = {
@@ -22,14 +29,14 @@ const rightColumnVariants: Variants = {
   visible: { opacity: 1, x: 0, transition: { duration: 0.6, ease: 'easeOut' } },
 };
 
-export function Faq() {
+export function Faq({title, hint, faqItems}: FAQProps) {
   return (    
     <section className="bg-white py-20 sm:py-28 overflow-x-hidden">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-x-12 gap-y-16 lg:grid-cols-2">
                     
           <motion.div
-            className="flex flex-col justify-center"
+            className="flex flex-col items-start"
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true, amount: 0.3 }}
@@ -38,14 +45,14 @@ export function Faq() {
             <div>
               <p className="text-base font-semibold leading-7 text-red-600">FAQ</p>
               <h2 className="max-w-[457px] mt-2 text-3xl font-bold tracking-wide leading-10 text-gray-900 sm:text-4xl">
-                Everything You Need to Know
+                {title}
               </h2>
-              <div className="mt-10">
+             {hint && ( <div className="mt-10">
                 <Link href="/faq" className="inline-flex items-center text-base font-semibold text-gray-800 hover:text-red-600 group">
-                  All Questions
+                  {hint}
                   <ArrowRight className="ml-2 h-5 w-5 transition-transform group-hover:translate-x-1" />
                 </Link>
-              </div>
+              </div>)}
             </div>
           </motion.div>
           
@@ -59,7 +66,7 @@ export function Faq() {
               {faqItems.map((item, index) => (
                 <AccordionItem key={index} value={`item-${index}`}>
                   <AccordionTrigger>{item.question}</AccordionTrigger>
-                  <AccordionContent>
+                  <AccordionContent className='py-2 px-4'>
                     {item.answer}
                   </AccordionContent>
                 </AccordionItem>
